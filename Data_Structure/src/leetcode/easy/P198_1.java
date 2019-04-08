8893ac2a26b896b86910965303a46dc6be34f1e0
UntilHell <1096322022@qq.com>
2019-04-08 22:00:16 +0800

    回溯

---
+++ Data_Structure/src/leetcode/easy/P198_1.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+public class P198_1 {
+
+	public int rob(int[] nums) {
+	   
+		 if(nums.length == 1) return nums[0];
+	       if(nums.length == 2) return Math.max(nums[0],nums[1]);
+			int odd = 0;
+			int even = 0;
+			for (int i = 0; i < nums.length; i+=2) {
+				odd += nums[i];
+			}
+			for (int i = 1; i < nums.length; i+=2) {
+				even += nums[i];
+			}
+			return Math.max(odd, even);
+	}
+ }
+
--- Data_Structure/src/leetcode/medium/P078.java
+++ Data_Structure/src/leetcode/medium/P078.java
@@ -1,10 +1,45 @@
 package leetcode.medium;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import org.junit.Test;
 
 public class P078 {
 
-//	public List<List<Integer>> subsets(int[] nums) {
-//        
-//    }
+	private List<List<Integer>> res = null;
+	public List<List<Integer>> subsets(int[] nums) {
+        
+		res = new LinkedList<>();
+		if(nums == null || nums.length == 0){
+			return res;
+		}
+		LinkedList<Integer> p = new LinkedList<>();
+		generateSubSet(nums,0,p);
+		return res;
+    }
+	private void generateSubSet(int[] nums, int index, LinkedList<Integer> p) {
+		
+//		if(index <= nums.length ){
+//			res.add((LinkedList<Integer>) p.clone());
+//			
+//			//return;
+//		}
+//		if(index == nums.length) return;
+		res.add((LinkedList<Integer>) p.clone());
+		for(int i = index; i < nums.length; i++){
+			p.addLast(nums[i]);
+			generateSubSet(nums, i +1, p);
+			p.removeLast();
+		}
+		
+	}
+	
+	@Test
+	public void test(){
+		int nums[] = {1,2,3};
+		subsets(nums);
+		
+	}
+	
 }
